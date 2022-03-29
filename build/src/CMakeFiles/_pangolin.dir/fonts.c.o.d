bf94fe4decc09c54c1d56f1745ca7a558c444451
AdityaNG <adityang5@gmail.com>
2022-03-29 22:19:10 +0530

    Removed build

--- build/src/CMakeFiles/_pangolin.dir/fonts.c.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/_pangolin.dir/fonts.c.o: \
- /home/aditya/VSProjects/pangolin/build/src/fonts.c \
- /usr/include/stdc-predef.h
--- build/src/CMakeFiles/_pangolin.dir/utils/timer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/_pangolin.dir/utils/timer.cpp.o: \
- /home/aditya/VSProjects/pangolin/src/utils/timer.cpp \
- /usr/include/stdc-predef.h
