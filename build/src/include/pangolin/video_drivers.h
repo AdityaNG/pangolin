a26feaa24f8f3c66fa82fa6f7b1a0d91be26acb5
AdityaNG <adityang5@gmail.com>
2022-03-29 22:16:30 +0530

    Hacky changes

---
+++ build/src/include/pangolin/video_drivers.h
@@ -0,0 +1,51 @@
+// CMake generated file. Do Not Edit.
+
+#pragma once
+
+namespace pangolin {
+
+void RegisterTestVideoFactory();
+void RegisterImagesVideoFactory();
+void RegisterImagesVideoOutputFactory();
+void RegisterSplitVideoFactory();
+void RegisterPvnVideoFactory();
+void RegisterPangoVideoFactory();
+void RegisterPangoVideoOutputFactory();
+void RegisterDebayerVideoFactory();
+void RegisterShiftVideoFactory();
+void RegisterMirrorVideoFactory();
+void RegisterUnpackVideoFactory();
+void RegisterJoinVideoFactory();
+void RegisterMergeVideoFactory();
+void RegisterJsonVideoFactory();
+void RegisterThreadVideoFactory();
+void RegisterFirewireVideoFactory();
+void RegisterV4lVideoFactory();
+void RegisterOpenNiVideoFactory();
+void RegisterOpenNi2VideoFactory();
+
+inline bool LoadBuiltInVideoDrivers()
+{
+    RegisterTestVideoFactory();
+    RegisterImagesVideoFactory();
+    RegisterImagesVideoOutputFactory();
+    RegisterSplitVideoFactory();
+    RegisterPvnVideoFactory();
+    RegisterPangoVideoFactory();
+    RegisterPangoVideoOutputFactory();
+    RegisterDebayerVideoFactory();
+    RegisterShiftVideoFactory();
+    RegisterMirrorVideoFactory();
+    RegisterUnpackVideoFactory();
+    RegisterJoinVideoFactory();
+    RegisterMergeVideoFactory();
+    RegisterJsonVideoFactory();
+    RegisterThreadVideoFactory();
+    RegisterFirewireVideoFactory();
+    RegisterV4lVideoFactory();
+    RegisterOpenNiVideoFactory();
+    RegisterOpenNi2VideoFactory();
+    return true;
+}
+
+} // pangolin
